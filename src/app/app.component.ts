6bb6d5c21ea1469864f0749b4037cec741cf8213
unknown <charleseduardofaria@gmail.com>
2024-03-25 09:46:40 -0300

    Two-way binding com ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Customer {
+  name: string;
+  email: string;
+  occupation: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  customer = new Customer();
+
   occupations = ['Engineer', 'Doctor', 'Designer', 'Artist', 'Teacher'];
 
-  occupation = 'Doctor';
-
   save(form: NgForm) {
-    console.log(form);
-    console.log(form.value.name);
+    // this.customer.name = form.value.name;
+    // this.customer.email = form.value.email;
+    // this.customer.occupation = form.value.occupation;
+
+    console.log(form.value);
+    console.log(this.customer);
   }
 }
